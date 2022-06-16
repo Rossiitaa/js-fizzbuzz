9c6549e47e07aad31c58e801743df0b06ec37179
Rossiitaa <rosario.scuotto@outlook.it>
2022-06-16 16:21:56 +0200

    variable creation and tags in html

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
-// creazione ciclo da 1 a 100
+// creazione variabile
+const consoleRow = document.querySelector('#console-to-html');
+
+// creazione ciclo da 1 a 100 di consoleRow
 for (let i = 1; i <= 100; i++)
 
 // settiamo le condizioni in cui se "i" è un moltiplicatore di 3 e/o 5 il console.log sarà diverso 
 if (i % 3 == 0 && i % 5 == 0) {
-    console.log('FizzBuzz');
+    
 
 } else if (i % 3 == 0) {
-    console.log("Fizz")
+    
 
 } else if (i % 5 == 0) {
-    console.log('Buzz')
+    
 
 } else {
     console.log (i)
