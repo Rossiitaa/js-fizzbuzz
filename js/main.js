04cc9bc64457c095faf574b924d7c0acd4de3ddc
Rossiitaa <rosario.scuotto@outlook.it>
2022-06-16 17:36:29 +0200

    bonus 1

--- js/main.js
+++ js/main.js
@@ -1,33 +1,48 @@
 // creazione ciclo da 1 a 100 di consoleRow
-for ( i = 1; i <= 100; i++); {
+for ( let i = 1; i <= 100; i++) {
 
 // creazione variabile
 const consoleRow = document.querySelector('#console-to-html');
 
 // settiamo le condizioni in cui se "i" è un moltiplicatore di 3 e/o 5 il console.log sarà diverso 
 if ( i % 3 == 0 && i % 5 == 0) {
+
     // creiamo in un div vuoto una variabile
     const BoxFB = "FizzBuzz"
     const boxFizzBuzz = document.createElement('div');
-
 // | aggiungiamo delle classi
-    boxFizzBuzz.classList.add('col-3', 'box', 'rounded');
-
+    
 //# aggiungiamo il contenuto nel div
     boxFizzBuzz.innerHTML = BoxFB;
-
 //° append
     consoleRow.append(boxFizzBuzz);
-
     console.log(BoxFB);
 
 } else if (i % 3 == 0) {
-    console.log("Fizz")
+
+    const fizz = "Fizz";
+    const fizzBox = document.createElement("div");
+    
+    fizzBox.innerHTML = fizz;
+    consoleRow.append(fizzBox);
+    console.log(fizz);
 
 } else if (i % 5 == 0) {
-    console.log('Buzz')
+
+    const buzz = "Buzz";
+    const buzzBox = document.createElement("div");
+    
+    buzzBox.innerHTML = buzz;
+    consoleRow.append(buzzBox);
+    console.log(buzz);
 
 } else {
-    console.log (i)
+
+    const box = document.createElement("div");
+    
+    box.innerHTML = i;
+    consoleRow.append(box);
+    console.log(i);
+    
 } 
 }
